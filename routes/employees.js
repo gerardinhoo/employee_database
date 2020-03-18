6b0fd98bbe54eafd6a8d85750ae2342e2dcf0e07
Gerard Eklu <gerard_eklu@homedepot.com>
2020-03-17 23:32:26 -0400

    WIP

--- routes/employees.js
+++ routes/employees.js
@@ -3,56 +3,93 @@ const router = express.Router();
 
 const Employee = require("../models/employee");
 
-
-
 // GET ROUTES
 router.get("/", (req, res) => {
-    Employee.find({})
-        .then(employees => {
-            res.render("index", { employees: employees });
-        })
-        .catch(err => {
-            console.log(err)
-        })
-})
+  Employee.find({})
+    .then(employees => {
+      res.render("index", { employees: employees });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 router.get("/employee/new", (req, res) => {
-    res.render("new")
-})
+  res.render("new");
+});
 
 router.get("/employee/search", (req, res) => {
-    res.render("search", { employee: "" })
-})
+  res.render("search", { employee: "" });
+});
 
 router.get("/employee", (req, res) => {
-    let searchQuery = { name: req.query.name };
+  let searchQuery = { name: req.query.name };
 
-    Employee.findOne(searchQuery)
-        .then(employee => {
-            res.render("search", { employee: employee })
-        })
-        .catch(err => {
-            console.log(err)
-        })
-})
+  Employee.findOne(searchQuery)
+    .then(employee => {
+      res.render("search", { employee: employee });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
-
+router.get("/edit/:id", (req, res) => {
+  let searchQuery = { _id: req.params.id };
+  Employee.findOne(searchQuery)
+    .then(employee => {
+      res.render("edit", { employee: employee });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 // POST ROUTES
 router.post("/employee/new", (req, res) => {
-    let newEmployee = {
-        name: req.body.name,
-        designation: req.body.designation,
-        salary: req.body.salary
-    }
-    Employee.create(newEmployee)
-        .then(employee => {
-            res.redirect("/")
-        })
-        .catch(err => {
-            console.log(err);
-        })
-})
+  let newEmployee = {
+    name: req.body.name,
+    designation: req.body.designation,
+    salary: req.body.salary,
+  };
+  Employee.create(newEmployee)
+    .then(employee => {
+      res.redirect("/");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
+// PUT ROUTE => MODIFY
+router.put("/edit/:id", (req, res) => {
+  let searchQuery = { _id: req.params.id };
+  Employee.updateOne(searchQuery, {
+    $set: {
+      name: req.body.name,
+      designation: req.body.designation,
+      salary: req.body.salary,
+    },
+  })
+    .then(employee => {
+      res.redirect("/");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+// DELETE ROUTE => DELETE FROM DATABASE
+router.delete("/delete/:id", (req, res) => {
+  let searchQuery = { _id: req.params.id };
+  Employee.deleteOne(searchQuery)
+    .then(employee => {
+      req.flash("success_msg", "Employee sucessfully deleted!");
+      res.redirect("/");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const app = express();
 const path = require("path");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const methodOverride = require('method-override');
+const session = require("express-session");
+const flash = require('connect-flash');
+
+
 
 const bodyParser = require("body-parser");
-
-
-
 
 const employeesRoutes = require("./routes/employees");
 
@@ -27,6 +29,27 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+// Middleware for method override
+app.use(methodOverride('_method'));
+
+
+// Middleware for express session
+app.use(session({
+    secret: "Dinho",
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Middleware for connect flash
+app.use(flash());
+
+// Setting messages variables globally
+app.use = ((req, res, next) => {
+    res.locals.success_msg = req.flash(("success_msg"));
+    res.locals.error_msg = req.flash(("error_msg"));
+    next();
+})
 
 
 app.use(employeesRoutes)
